5d7e3577caef4bd7cc3b4000fd0956b044e02847
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-07-23 15:27:11 +0500

    add author quiz component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
-let model = {clicks:5};
-
+// APP.js
+/*let model = {clicks:5};
 function render(){
-    ReactDOM.render(<App clicks={model.clicks} onClick={()=>{model.clicks += 1; render()}} />, document.getElementById('root'));
+    ReactDOM.render(<App 
+        clicks={model.clicks} 
+        onClick={()=>{model.clicks += 1; render()}} />, document.getElementById('root'));
 }
+render();*/
 
-render();
+// AuthorQuiz.js
+
+ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
 serviceWorker.unregister();
